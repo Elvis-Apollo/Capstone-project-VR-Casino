f800d1a7093435c8e8ce05225c52fbf720fd57a8
aleemulhaq <aleemulhaq@gmail.com>
2020-01-31 16:52:35 -0500

    Adding timer to lever

--- CasinoVR/Assets/Lever.cs
+++ CasinoVR/Assets/Lever.cs
@@ -9,9 +9,22 @@ public class Lever : MonoBehaviour
     Reel reelc;
     Reel reeld;
 
-    void OnMouseDown()
+    private IEnumerator coroutine;
+
+    IEnumerator OnMouseDown()
     {
+
         
+            // - After 0 seconds, prints "Starting 0.0"
+            // - After 2 seconds, prints "WaitAndPrint 2.0"
+            // - After 2 seconds, prints "Done 2.0"
+            print("Starting " + Time.time);
+
+            // Start function WaitAndPrint as a coroutine. And wait until it is completed.
+            // the same as yield return WaitAndPrint(2.0f);
+            yield return StartCoroutine(WaitAndPrint(2.0f));
+            print("Done " + Time.time);
+    
         Debug.Log("Mouse left click pressed.");
 
         //changes reel values to 8
@@ -28,9 +41,16 @@ public class Lever : MonoBehaviour
     }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
+    }
+
+    // suspend execution for waitTime seconds
+    IEnumerator WaitAndPrint(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        print("WaitAndPrint " + Time.time);
     }
 
     // Update is called once per frame
