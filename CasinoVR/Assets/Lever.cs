b2cf35e5ad625c4241db74a86cdb5e456aac887f
aleemulhaq <aleemulhaq@gmail.com>
2020-01-31 15:45:27 -0500

    Adding bunch of basic c# scritpts on demo slotmachine

---
+++ CasinoVR/Assets/Lever.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    ReelA reela;
+    ReelA reelb;
+
+    void OnMouseDown()
+    {
+        
+        Debug.Log("Mouse left click pressed.");
+        reela = GameObject.Find("ReelA").GetComponent<ReelA>();
+
+        reela.TestReel();
+        reelb = GameObject.Find("ReelB").GetComponent<ReelA>();
+        reelb.TestReel();
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        
+    }
+}
---
+++ CasinoVR/Assets/ReelA.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReelA : MonoBehaviour 
+{
+    
+    public void TestReel()
+    {
+        GetComponent<TextMesh>().text = "8";
+        Debug.Log("Reel change from lever works!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
