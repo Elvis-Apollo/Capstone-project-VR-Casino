47b38d4f012286c598e1c420e7451e678dfd9734
aleemulhaq <aleemulhaq@gmail.com>
2020-02-08 16:22:52 -0500

    Adding latest version of proj

---
+++ CasinoVR/Assets/Reel_Primary_Spin.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reel_Primary_Spin : MonoBehaviour
+{
+
+    public Sprite[] sprites;
+
+    public GameObject leverObject;
+
+    public Sprite finalSprite;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //RandomingImage();
+        if (leverObject.GetComponent<Lever_Trigger>().spinReels == true)
+        {
+            RandomingImage();
+        }
+    }
+
+    void RandomingImage()
+    {
+        finalSprite = gameObject.GetComponent<UnityEngine.UI.Image>().sprite = sprites[Random.Range(0, sprites.Length)];
+    }
+
+    public void EndRand()
+    {
+        enabled = false;
+    }
+
+    // to disable script after stop time
+    /*public void StopRand()
+	{
+		Invoke("EndRand", stopTime);
+
+	}*/
+
+    public void StartRand()
+    {
+        enabled = true;
+    }
+}
