b1f50f886c91e0b2274ba0f0222ee5f0f351f90a
aleemulhaq <aleemulhaq@gmail.com>
2020-03-17 13:52:19 -0400

    Adding Casino Admin form app

---
+++ CasinoAdmin/CasinoAdmin/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CasinoAdmin
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        // create holt laury form
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // create holt laury form 
+            HoltLauryForm hl_form = new HoltLauryForm();
+            hl_form.CreateHoltLauryForm();
+            //this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // create admin form 
+            AdminForm admin_form = new AdminForm();
+            admin_form.CreateAdminForm();            
+        }
+    }
+}
---
+++ CasinoAdmin/CasinoAdmin/Form2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CasinoAdmin
+{
+    public partial class AdminForm : Form
+    {
+        public AdminForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void CreateAdminForm()
+        {
+            // Create a new instance of the form.
+            Form form1 = new Form();
+            // Create two buttons to use as the accept and cancel buttons.
+            Button ok_btn = new Button();
+            Button cancel_btn = new Button();
+
+            // Set the text of button1 to "OK".
+            ok_btn.Text = "OK";
+            ok_btn.Click += okButtonClick;
+            // Set the position of the button on the form.
+            ok_btn.Location = new Point(10, 10);
+            // Set the text of button2 to "Cancel".
+            cancel_btn.Text = "Cancel";
+            // Set the position of the button based on the location of button1.
+            cancel_btn.Location
+               = new Point(ok_btn.Left, ok_btn.Height + ok_btn.Top + 10);
+            // Set the caption bar text of the form.   
+            form1.Text = "Admin Portal";
+            // Display a help button on the form.
+            form1.HelpButton = true;
+
+            // Define the border style of the form to a dialog box.
+            form1.FormBorderStyle = FormBorderStyle.FixedDialog;
+            // Set the MaximizeBox to false to remove the maximize box.
+            form1.MaximizeBox = false;
+            // Set the MinimizeBox to false to remove the minimize box.
+            form1.MinimizeBox = false;
+            // Set the accept button of the form to button1.
+            form1.AcceptButton = ok_btn;
+            // Set the cancel button of the form to button2.
+            form1.CancelButton = cancel_btn;
+            // Set the start position of the form to the center of the screen.
+            form1.StartPosition = FormStartPosition.CenterScreen;
+
+            // Add button1 to the form.
+            form1.Controls.Add(ok_btn);
+            // Add button2 to the form.
+            form1.Controls.Add(cancel_btn);
+
+            // Display the form as a modal dialog box.
+            form1.ShowDialog();
+        }
+
+        private void okButtonClick(object sender, EventArgs e)
+        {
+            Console.WriteLine("OK button clicked");
+        }
+    }
+}
