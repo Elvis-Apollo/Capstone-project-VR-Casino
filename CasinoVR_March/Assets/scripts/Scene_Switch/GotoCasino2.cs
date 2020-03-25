7aa402159d9d4dde23cb178c281b79ded8944b79
Elvis-Apollo <chenz81@mcmaster.ca>
2020-03-25 16:20:10 -0400

    added SceneSwitcher function and multiple scenes

---
+++ CasinoVR_March/Assets/scripts/Scene_Switch/GotoCasino2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * Author: Elvis Chen
+ * Date: March 2020
+ * 
+ * This script loads scene casino2
+ */
+public class GotoCasino2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SceneManager.LoadScene("Casino2");
+	}
+	
+	
+}
---
+++ CasinoVR_March/Assets/scripts/Scene_Switch/SceneLoadinator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * Author: Elvis Chen
+ * Date: March 2020
+ * 
+ * This script loads scene casino1
+ */
+public class SceneLoadinator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SceneManager.LoadScene("Casino1");
+	}
+	
+	
+		
+}
